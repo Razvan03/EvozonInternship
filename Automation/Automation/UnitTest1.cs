b641d8a92d890d36fedcc5ab5638f74db62c246f
Roman Razvan <roman_razvan03@yahoo.com>
2023-08-04 09:58:38 +0300

    Test (#12)

--- Automation/Automation/UnitTest1.cs
+++ Automation/Automation/UnitTest1.cs
@@ -15,23 +15,8 @@ namespace Automation
     {
         #region Login && Register
 
-        [TestMethod]
-        public void LoginTest()
-        {
-            WebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("http://qa3magento.dev.evozon.com/");
-            driver.FindElement(By.CssSelector("#header > div > div.skip-links > div > a > span.label")).Click(); //Account button
-            driver.FindElement(By.CssSelector("#header-account > div > ul > li.last > a")).Click(); //Login Button
-            driver.FindElement(By.Id("email")).SendKeys("roman_razvan03@yahoo.com");
-            driver.FindElement(By.Id("pass")).SendKeys("tester1");
-            driver.FindElement(By.Id("send2")).Click(); //Login submit
-            String welcomeText = driver.FindElement(By.CssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong")).Text; //Hello Text
-
-            welcomeText.Should().Be("Hello, Roman Razvan!");
-
-            driver.Close();
-        }
+    
+      
 
         [TestMethod]
         public void RegisterTest()
