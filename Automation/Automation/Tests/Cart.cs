a5c709119e73a333297d39a0489e733e4ca4da89
csaba.gyarmathy <csaba.gyarmathy@evozon.com>
2023-08-08 16:02:23 +0300

    Removed unnecessary method from cart tests

--- Automation/Automation/Tests/Cart.cs
+++ Automation/Automation/Tests/Cart.cs
@@ -9,24 +9,24 @@ namespace Automation.Tests
     [TestClass]
     public class Cart : BaseTest
     {
-        public string productAddedToCart;
+        public string productName;
 
         [TestMethod]
         public void AddtoCartConfigurableProductTest()
         {
             Pages.HomePage.GoToSubcategoryFromDropdown(Category.MEN, Subcategory.Men.TEES_KNITS_AND_POLOS);
 
-            Pages.ProductsPage.GoToProductDetailsPage("Chelsea Tee");
+            productName = "Chelsea Tee";
 
-            productAddedToCart = Pages.ProductDetailPage.GetProductName();
+            Pages.ProductsPage.GoToProductDetailsPage(productName);
 
             Pages.ProductDetailPage.SelectItemColor(Color.Black);
             Pages.ProductDetailPage.SelectItemSize(ClothesSize.M);
             Pages.ProductDetailPage.ChangeQty();
             Pages.ProductDetailPage.AddProductToCart();
 
-            Pages.CartPage.IsConfirmMessageTrue(productAddedToCart).Should().BeTrue();
-            Pages.CartPage.IsProductInCart(productAddedToCart).Should().BeTrue();
+            Pages.CartPage.IsConfirmMessageTrue(productName).Should().BeTrue();
+            Pages.CartPage.IsProductInCart(productName).Should().BeTrue();
 
         }
 
@@ -35,9 +35,9 @@ namespace Automation.Tests
         {
             Pages.HomePage.GoToSubcategoryFromDropdown(Category.HOME_AND_DECOR, Subcategory.HomeAndDecor.BOOKS_AND_MUSIC);
 
-            Pages.ProductsPage.GoToProductDetailsPage("A Tale of Two Cities");
+            productName = "A Tale of Two Cities";
 
-            productAddedToCart = Pages.ProductDetailPage.GetProductName();
+            Pages.ProductsPage.GoToProductDetailsPage(productName);
 
             Pages.ProductDetailPage.ChangeQty();
 
@@ -45,9 +45,9 @@ namespace Automation.Tests
 
             Pages.ProductDetailPage.AddProductToCart();
 
-            Pages.CartPage.IsConfirmMessageTrue(productAddedToCart).Should().BeTrue();
+            Pages.CartPage.IsConfirmMessageTrue(productName).Should().BeTrue();
 
-            Pages.CartPage.IsProductInCart(productAddedToCart).Should().BeTrue();
+            Pages.CartPage.IsProductInCart(productName).Should().BeTrue();
 
         }
 
@@ -56,23 +56,23 @@ namespace Automation.Tests
         {
             Pages.HomePage.GoToSubcategoryFromDropdown(Category.VIP, null);
 
-            Pages.ProductsPage.GoToProductDetailsPage("Broad St. Flapover Briefcase");
+            productName = "Broad St. Flapover Briefcase";
 
-            productAddedToCart = Pages.ProductDetailPage.GetProductName();
+            Pages.ProductsPage.GoToProductDetailsPage(productName);
 
             Pages.ProductDetailPage.ChangeQty();
 
             Pages.ProductDetailPage.AddProductToCart();
 
-            Pages.CartPage.IsConfirmMessageTrue(productAddedToCart).Should().BeTrue();
+            Pages.CartPage.IsConfirmMessageTrue(productName).Should().BeTrue();
 
-            Pages.CartPage.IsProductInCart(productAddedToCart).Should().BeTrue();
+            Pages.CartPage.IsProductInCart(productName).Should().BeTrue();
         }
 
         [TestCleanup]
         public void AddtoCartTestCleanup()
         {
-            Pages.CartPage.RemoveProductFromCart(productAddedToCart);
+            Pages.CartPage.RemoveProductFromCart(productName);
         }
     }
 }
